fe6a797ccdfd222380ab0fedc9b425b6908b6cbf
Daria Kostenko <dolcevitaforever95@gmail.com>
2020-08-15 16:24:50 +0300

    module 1 complete

---
+++ Module 1. Basics. Node.js fundamental theory/scripts/task2.js
@@ -0,0 +1,26 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+
+const inputFile = './sources/input.csv';
+const outputFile = './sources/output.txt';
+
+const readStream = fs.createReadStream(inputFile);
+const writeStream = fs.createWriteStream(outputFile);
+
+readStream
+    .on('error', (error) => console.error(`Reading csv error\n${error.message}`))
+    .pipe(csv())
+    .on('error', (error) => console.error(`Converting csv to txt error\n${error.message}`))
+    .pipe(writeStream)
+    .on('error', (error) => console.error(`Writing to txt error\n${error.message}`))
+    .on('finish', () => console.log('Converting has been compleated. Take a look at your new file.'));
+
+// line by line
+// csv()
+// .fromStream(readStream)
+// .subscribe((json) => {
+// 	writeStream.write(`${JSON.stringify(json)}\n`);
+// });
+
+// readStream.on('error', (error) => console.error(`Reading csv error\n${error.message}`));
+// writeStream.on('error', (error) => console.error(`Writing to txt error\n${error.message}`))
---
+++ Module 1. Basics. Node.js fundamental theory/utils/string-revers_es6.js
@@ -0,0 +1,1 @@
+export const reversString = str => str.split('').reverse().join('');
